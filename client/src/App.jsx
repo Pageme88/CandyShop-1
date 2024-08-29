0651994ceabc472611894bf3f95861e20393cd84
Jason <jasoncweimar@gmail.com>
2024-08-29 00:44:49 -0600

    Integrated all components & Route paths - note: Refactored App.jsx to use RESTful naming conventions for URL paths.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginAndRegView from './views/LoginAndRegView';
+import PlaceOrder from './components/PlaceOrder';
+import CandyDetails from './components/CandyDetails';
+import CheckOut from './components/Checkout';
+import MenuPage from './components/Candies';
+import Recipes from './components/Recipes';
+import RecipeDetails from './components/RecipeDetails';
 import HomePageView from './views/HomePageView';
+
 
 
 function App() {
@@ -8,8 +15,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/candyshop" element={<HomePageView />} />
-        <Route path="/candyshop/auth" element={<LoginAndRegView />} />
+        <Route path="/candyshop" element={<LoginAndRegView />} />
+        <Route path="/candyshop/home" element={<HomePageView />} />
+        <Route path="/candyshop/placeorder" element={<PlaceOrder />} />
+        <Route path="/candyshop/candies" element={<MenuPage />} />
+        <Route path="/candyshop/candy/:id" element={<CandyDetails />} />
+        <Route path="/candyshop/recipes" element={<Recipes />} />
+        <Route path="/candyshop/recipes/:id" element={<RecipeDetails />} />
+        <Route path="/candyshop/checkout" element={<CheckOut />} />
       </Routes>
     </BrowserRouter>
   )
